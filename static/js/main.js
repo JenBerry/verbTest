1a879c184205d192fe52b4433729c2bf95eb0983
Jen Berry <jenberrymail@gmail.com>
2014-02-17 16:27:41 +0000

    responsive homescreen, header and footer

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-	console.log("Hello!");
+	//show and hide the menu on smaller screens
+	$('.menu').on('click', function(){
+		var $nav = $('nav')
+		if ($nav.is(':hidden')){
+			$('nav').slideDown("slow");
+		}
+		else
+		{
+			$('nav').slideUp("slow", function(){
+				$('nav').removeAttr( 'style' );
+				//make sure the menu becomes visible again
+				//if the screen width is increased
+			});
+		}
+	});
 });
